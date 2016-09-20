e05907ed112f96df565fe7f4e515a4d2fcd5dd0f
Lohan Bodevan <lohan.bodevan@gmail.com>
2016-09-20 03:31:10 -0300

    Add address to supplier model

--- supplier/models.py
+++ supplier/models.py
@@ -5,6 +5,7 @@ class Supplier(models.Model):
     name = models.CharField(max_length=50, blank=False)
     phone = models.CharField(max_length=20, blank=True)
     email = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=150, blank=True)
 
     def __str__(self):
         return self.name
